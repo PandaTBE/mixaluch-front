7f27e02be234a1b2a8d424a8f037bf52083d121f
Nikita Surov <surov.n2012@gmail.com>
2022-11-16 21:09:53 +0300

    фикс краша

--- components/Catalog/hooks/usePrepareData.ts
+++ components/Catalog/hooks/usePrepareData.ts
@@ -1,17 +1,17 @@
-import { categoryState } from './../../../slices/Category/category';
+import { categoryReducerValues } from './../../../slices/Category/category';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { storeCategoriesByParentId } from '../../../slices/Catalog/catalog';
 import { ICategoriesByParentId } from '../../../slices/Catalog/interfaces';
 
 const usePrepareData = () => {
-    const { categories } = useSelector(categoryState);
+    const { categories } = useSelector(categoryReducerValues);
     const dispatch = useDispatch();
 
     useEffect(() => {
         if (categories) {
             const result = categories.reduce((acc: ICategoriesByParentId, value) => {
-                if (value.level === 0) {
+                if (value.parent === null) {
                     const children = categories.filter((element) => element.parent === value.id);
                     acc[value.id] = {
                         parent: value,
