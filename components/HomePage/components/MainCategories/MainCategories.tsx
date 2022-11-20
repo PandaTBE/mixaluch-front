5ba880e9213a9dcba294fea749e232f2e1989a58
Nikita Surov <surov.n2012@gmail.com>
2022-11-20 20:15:14 +0300

    Редирект с главной страницы

--- components/HomePage/components/MainCategories/MainCategories.tsx
+++ components/HomePage/components/MainCategories/MainCategories.tsx
@@ -1,6 +1,9 @@
 import { Grid } from '@mui/material';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { storeSelectedCategoryId } from '../../../../slices/Catalog/catalog';
 import { categoryReducerValues } from '../../../../slices/Category/category';
+import { storePageToSwitch } from '../../../../slices/General/general';
+import { TPageToSwitch } from '../../../../slices/General/interfaces';
 import CategoryCard from './components/CategoryCard/CategoryCard';
 import { Wrapper } from './styles';
 
@@ -9,13 +12,27 @@ import { Wrapper } from './styles';
  */
 const MainCategories = () => {
     const { mainCategories } = useSelector(categoryReducerValues);
+    const dispatch = useDispatch();
+
+    const storePageToSwitchTrans = (page: TPageToSwitch) => {
+        dispatch(storePageToSwitch(page));
+    };
+
+    const storeSelectedCategoryIdTrans = (id: number | null) => {
+        dispatch(storeSelectedCategoryId(id));
+    };
+
     return (
         <Wrapper>
             <Grid container spacing={2}>
                 {mainCategories?.map((category) => {
                     return (
                         <Grid xs={6} item key={category.id}>
-                            <CategoryCard category={category} />
+                            <CategoryCard
+                                storeSelectedCategoryIdTrans={storeSelectedCategoryIdTrans}
+                                storePageToSwitchTrans={storePageToSwitchTrans}
+                                category={category}
+                            />
                         </Grid>
                     );
                 })}
--- components/HomePage/components/MainCategories/components/CategoryCard/CategoryCard.tsx
+++ components/HomePage/components/MainCategories/components/CategoryCard/CategoryCard.tsx
@@ -1,14 +1,32 @@
+import { useRouter } from 'next/router';
 import { FC } from 'react';
 import { ICategory } from '../../../../../../models/Category';
+import { TPageToSwitch } from '../../../../../../slices/General/interfaces';
 import { CategoryImage, NameWrapper, Wrapper } from './styles';
 
 interface IProps {
+    /** Данные категории */
     category: ICategory;
+    /** Функция для записи выбранной категории */
+    storeSelectedCategoryIdTrans: (id: number | null) => void;
+    /** Функция для записи страницы, на которую происходит редирект */
+    storePageToSwitchTrans: (page: TPageToSwitch) => void;
 }
 
-const CategoryCard: FC<IProps> = ({ category }) => {
+/**
+ * Компонент для отображения карточки с главной категорией
+ */
+const CategoryCard: FC<IProps> = ({ category, storeSelectedCategoryIdTrans, storePageToSwitchTrans }) => {
+    const router = useRouter();
+
+    const onCardClick = () => {
+        storeSelectedCategoryIdTrans(category.id);
+        storePageToSwitchTrans('/catalog');
+        router.push('/catalog');
+    };
+
     return (
-        <Wrapper>
+        <Wrapper onClick={onCardClick}>
             <CategoryImage src={category.image} />
             <NameWrapper>{category.name}</NameWrapper>
         </Wrapper>
