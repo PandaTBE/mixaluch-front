25597d67956f9ca591f13e33fe077295c37d07ac
Nikita Surov <surov.n2012@gmail.com>
2022-11-22 15:27:59 +0300

    Новая логика каталога

--- api/intex.ts
+++ api/intex.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: 'http://192.168.1.50:80/api/',
+    baseURL: 'http://127.0.0.1:8000/api/',
 });
 
 // 192.168.1.50:80
