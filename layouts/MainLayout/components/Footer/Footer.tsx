a5e5e23dba79448c95f33ea69cbb1fb0d9afe80b
Nikita Surov <surov.n2012@gmail.com>
2023-02-03 19:10:29 +0300

    Отображение подвала страницы

--- layouts/MainLayout/components/Footer/Footer.tsx
+++ layouts/MainLayout/components/Footer/Footer.tsx
@@ -1,14 +1,45 @@
+import { Stack } from '@mui/material';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux';
 import Container from '../../../../components/Container/Container';
-import { Wrapper } from './styles';
+import { storePageToSwitch } from '../../../../slices/General/general';
+import { TPageToSwitch } from '../../../../slices/General/interfaces';
+import { navigationListItems } from '../constants/constants';
+import { ContentWrapper, StyledLink, Wrapper, Delivery, Logo } from './styles';
 
 /**
  * Компонент для отображения подвала страницы
  */
 const Footer = () => {
+    const dispatch = useDispatch();
+    const router = useRouter();
+
+    const onLinkClick = (link: TPageToSwitch) => () => {
+        dispatch(storePageToSwitch(link));
+    };
+
     return (
         <Wrapper>
             <Container>
-                <div>footer</div>
+                <ContentWrapper>
+                    <Stack direction={'row'} gap={2} flexWrap={'wrap'} justifyContent={'center'} alignItems={'center'}>
+                        {navigationListItems.map((element) => {
+                            return (
+                                <StyledLink
+                                    key={element.id}
+                                    data-targetid={element.id}
+                                    onClick={onLinkClick(element.href as TPageToSwitch)}
+                                    active={router.pathname === element.href}
+                                >
+                                    <Link href={element.href}>{element.name}</Link>
+                                </StyledLink>
+                            );
+                        })}
+                    </Stack>
+                    <Delivery>Доставка с 9:00 до 19:00</Delivery>
+                    <Logo onClick={onLinkClick('/')} src={'/logo.png'} alt={'Mixaluch logo'} />
+                </ContentWrapper>
             </Container>
         </Wrapper>
     );
--- layouts/MainLayout/components/Footer/styles.ts
+++ layouts/MainLayout/components/Footer/styles.ts
@@ -4,3 +4,31 @@ export const Wrapper = styled.div`
     background-color: ${(p) => p.theme.backgroundColors.primary};
     min-height: 100px;
 `;
+
+export const StyledLink = styled.div<{ active: boolean }>`
+    color: ${(props) => (props.active ? props.theme.colors.primary : props.theme.colors.black)};
+    white-space: nowrap;
+`;
+
+export const ContentWrapper = styled.div`
+    padding: 15px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const Delivery = styled.div`
+    color: ${(p) => p.theme.colors.grey};
+    white-space: nowrap;
+    margin-top: 15px;
+`;
+
+export const Logo = styled.img`
+    width: 220px;
+    cursor: pointer;
+    margin-top: 15px;
+
+    @media (max-width: 575px) {
+        width: 150px;
+    }
+`;
