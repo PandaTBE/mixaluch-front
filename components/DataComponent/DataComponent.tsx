d32411bec353e5b0d2a3b989d4b8edbb0dad5e96
Nikita Surov <surov.n2012@gmail.com>
2023-02-09 23:57:05 +0300

    123

--- components/DataComponent/DataComponent.tsx
+++ components/DataComponent/DataComponent.tsx
@@ -6,9 +6,9 @@ import { useGetRawData as useCartRawData } from '../pages/CartPage/hooks/useFetc
  * Функция для получения начальных данных
  */
 const DataComponent = () => {
-    useCartRawData();
-    usePrepareData();
-    useGetUserData();
+    // useCartRawData();
+    // usePrepareData();
+    // useGetUserData();
     return null;
 };
 
