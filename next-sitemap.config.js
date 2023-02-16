a48bffbb0a33154ad0b614d77d5c9a73259a4edc
Nikita Surov <surov.n2012@gmail.com>
2023-02-16 18:57:09 +0300

    настройка конфигурации

---
+++ next-sitemap.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next-sitemap').IConfig} */
+
+// eslint-disable-next-line no-undef
+module.exports = {
+    siteUrl: 'https://mixaluch-shop-dev.ru/',
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+    generateRobotsTxt: true,
+    exclude: ['/cart', '/register', '/login', '/activate', '/orders', '/catalog', '/user-account'],
+    robotsTxtOptions: {
+        policies: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+            {
+                userAgent: 'test-bot',
+                allow: ['/contacts', '/about', '/delivery'],
+            },
+            {
+                userAgent: 'black-listed-bot',
+                disallow: ['/cart', '/register', '/login', '/activate', '/orders', '/catalog', '/user-account'],
+            },
+        ],
+    },
+};
