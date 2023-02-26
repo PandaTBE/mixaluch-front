e5f6a1ff14dc37efc0561c8bce92b9bf8fc10450
Nikita Surov <surov.n2012@gmail.com>
2023-02-26 18:36:10 +0300

    аналитика

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,18 +32,16 @@ class MyDocument extends Document {
         return (
             <Html lang="ru">
                 <Head>
-                    <Script>
-                        {`(function (w, d, s, l, i) {
-                        w[l] = w[l] || [];
-                        w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-                        var f = d.getElementsByTagName(s)[0],
-                            j = d.createElement(s),
-                            dl = l != 'dataLayer' ? '&l=' + l : '';
-                        j.async = true;
-                        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-                        f.parentNode.insertBefore(j, f);
-                    })(window, document, 'script', 'dataLayer', 'GTM-5F5V3MX')`}
-                    </Script>
+                    <Script
+                        strategy="afterInteractive"
+                        dangerouslySetInnerHTML={{
+                            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5F5V3MX');`,
+                        }}
+                    ></Script>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link
@@ -53,14 +51,12 @@ class MyDocument extends Document {
                     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
                 </Head>
                 <body>
-                    <noscript>
-                        <iframe
-                            src="https://www.googletagmanager.com/ns.html?id=GTM-5F5V3MX"
-                            height="0"
-                            width="0"
-                            style={{ display: 'none', visibility: 'hidden' }}
-                        ></iframe>
-                    </noscript>
+                    <noscript
+                        dangerouslySetInnerHTML={{
+                            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5F5V3MX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+                        }}
+                    ></noscript>
                     <Main />
                     <NextScript />
                 </body>
