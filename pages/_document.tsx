9750d24b998e9d5d5df64b52bda3808013710b9c
Nikita Surov <surov.n2012@gmail.com>
2023-02-26 18:20:50 +0300

    аналитика гугл

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 import { ServerStyleSheet } from 'styled-components';
 
@@ -31,6 +32,18 @@ class MyDocument extends Document {
         return (
             <Html lang="ru">
                 <Head>
+                    <Script>
+                        {`(function (w, d, s, l, i) {
+                        w[l] = w[l] || [];
+                        w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+                        var f = d.getElementsByTagName(s)[0],
+                            j = d.createElement(s),
+                            dl = l != 'dataLayer' ? '&l=' + l : '';
+                        j.async = true;
+                        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                        f.parentNode.insertBefore(j, f);
+                    })(window, document, 'script', 'dataLayer', 'GTM-5F5V3MX')`}
+                    </Script>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link
@@ -40,6 +53,14 @@ class MyDocument extends Document {
                     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
                 </Head>
                 <body>
+                    <noscript>
+                        <iframe
+                            src="https://www.googletagmanager.com/ns.html?id=GTM-5F5V3MX"
+                            height="0"
+                            width="0"
+                            style={{ display: 'none', visibility: 'hidden' }}
+                        ></iframe>
+                    </noscript>
                     <Main />
                     <NextScript />
                 </body>
