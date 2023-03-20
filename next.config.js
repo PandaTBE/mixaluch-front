ad649fd2e1f87f48dd782fa12c945ec0615eeca1
Nikita Surov <surov.n2012@gmail.com>
2023-03-20 22:02:52 +0300

    213

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     swcMinify: true,
 
     images: {
-        domains: ['127.0.0.1', 'mixaluch-back.ru', 'www.mixaluch-back.ru', 'localhost'],
+        domains: ['127.0.0.1', 'mixaluch-back.ru', 'www.mixaluch-back.ru', 'localhost', 'mixaluch-shop-dev.ru'],
         loader: 'default',
     },
 };
